4d6e98d5eccafba7169cd93127e5cab2bbd02d97
IloveNooodles <mgarebaldhie80@gmail.com>
2022-11-30 02:02:51 +0700

    feat: logging and subscription service

--- src/main/java/service/LoggingService.java
+++ src/main/java/service/LoggingService.java
@@ -1,4 +1,33 @@
 package service;
 
+import enums.ApiResp;
+import model.LoggingModel;
+import repository.LoggingRepository;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoggingService {
+    private final LoggingRepository loggingRepository = new LoggingRepository();
+
+    public List<LoggingModel> getAllLoging(){
+        try {
+            List<LoggingModel> llm = loggingRepository.getAllLogging();
+            return llm;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    public String addLoging(LoggingModel lm){
+        try {
+            loggingRepository.addLogging(lm);
+            return ApiResp.LOGGING_SUCCESFUL;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return ApiResp.INTERNAL_SERVER_ERROR;
+    }
 }
--- src/main/java/service/SubscriptionService.java
+++ src/main/java/service/SubscriptionService.java
@@ -1,4 +1,118 @@
 package service;
 
+import enums.ApiResp;
+import enums.Status;
+import model.SubscriptionModel;
+import repository.SubscriptionRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class SubscriptionService {
+    private final SubscriptionRepository subscriptionRepository = new SubscriptionRepository();
+
+    public String subscribe(int creator_id, int subscriber_id){
+        // TODO check kalo creator id atau subscriber id nya gaada
+        if(creator_id < 0){
+            return ApiResp.INVALID_CREATOR_ID;
+        }
+
+        if(subscriber_id < 0){
+            return ApiResp.INVALID_SUBSCRIBER_ID;
+        }
+
+        try {
+
+            SubscriptionModel sm = subscriptionRepository.getSubscriptionById(creator_id, subscriber_id);
+            if(sm != null){
+                return ApiResp.ALREADY_SUBSCRIBE;
+            }
+
+            subscriptionRepository.subscribe(creator_id, subscriber_id);
+            return ApiResp.SUBSCRIPTION_SUCCESSFUL;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return ApiResp.INTERNAL_SERVER_ERROR;
+    }
+
+
+    public String checkStatus(int creator_id, int subscriber_id){
+
+        if(creator_id < 0){
+            return ApiResp.INVALID_CREATOR_ID;
+        }
+
+        if(subscriber_id < 0){
+            return ApiResp.INVALID_SUBSCRIBER_ID;
+        }
+
+        try {
+            //TODO ini polling belom kebayang apa ja yang mau di hit
+            SubscriptionModel sm = subscriptionRepository.getSubscriptionById(creator_id, subscriber_id);
+            if(sm == null){
+                return ApiResp.SUBSCRIPTION_NOT_FOUND;
+            }
+            return sm.getStatus().toString();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return ApiResp.INTERNAL_SERVER_ERROR;
+    }
+
+    public String rejectSubscription(int creator_id, int subscriber_id){
+
+        if(creator_id < 0){
+            return ApiResp.INVALID_CREATOR_ID;
+        }
+
+        if(subscriber_id < 0){
+            return ApiResp.INVALID_SUBSCRIBER_ID;
+        }
+
+        try {
+            SubscriptionModel sm = subscriptionRepository.getSubscriptionById(creator_id, subscriber_id);
+            if(sm == null){
+                return ApiResp.SUBSCRIPTION_NOT_FOUND;
+            }
+            subscriptionRepository.updateSubscription(creator_id, subscriber_id, Status.REJECTED);
+            return ApiResp.REJECT_SUBSCRIPTION;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return ApiResp.INTERNAL_SERVER_ERROR;
+    }
+
+    public String acceptSubscription(int creator_id, int subscriber_id){
+
+        if(creator_id < 0){
+            return ApiResp.INVALID_CREATOR_ID;
+        }
+
+        if(subscriber_id < 0){
+            return ApiResp.INVALID_SUBSCRIBER_ID;
+        }
+
+        try {
+            SubscriptionModel sm = subscriptionRepository.getSubscriptionById(creator_id, subscriber_id);
+            if(sm == null){
+                return ApiResp.SUBSCRIPTION_NOT_FOUND;
+            }
+            subscriptionRepository.updateSubscription(creator_id, subscriber_id, Status.ACCEPTED);
+            return ApiResp.ACCEPT_SUBSCRIPTION;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return ApiResp.INTERNAL_SERVER_ERROR;
+    }
+
+    public List<SubscriptionModel> fetchPendingSubscription(){
+        try {
+            List<SubscriptionModel> result = subscriptionRepository.getSubscriptionByStatus(Status.PENDING);
+            return result;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
